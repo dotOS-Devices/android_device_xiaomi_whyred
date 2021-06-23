c58f2659727753acf8aed9a7ca975d05ce377d2c
Sipun Ku Mahanta <sipunkumar85@gmail.com>
2021-06-23 21:05:09 +0530

    Droid on time bringup
    Signed-off-by: Sipun Ku Mahanta <sipunkumar85@gmail.com>
    Change-Id: Ib90818662c4a8e0a2935a5e497ea39977ab13fca

--- dot_whyred.mk
+++ dot_whyred.mk
@@ -19,8 +19,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
 
-# Inherit some common Superior stuff
-$(call inherit-product, vendor/superior/config/common.mk)
+# Inherit some common dotOS stuff
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit from custom vendor
 $(call inherit-product, vendor/MiuiCamera/config.mk)
@@ -31,7 +31,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := superior_whyred
+PRODUCT_NAME := dot_whyred
 PRODUCT_MODEL := Redmi Note 5
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
